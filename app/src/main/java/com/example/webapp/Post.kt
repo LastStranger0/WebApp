49558fd5e7ea4313a064dc13325dc2272131df5b
Anton Schur <aschur@scislt.com>
2022-12-12 18:51:16 +0300

    init

---
+++ app/src/main/java/com/example/webapp/Post.kt
@@ -0,0 +1,19 @@
+package com.example.webapp
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity
+data class Post(
+    @SerializedName("body")
+    var body: String = "",
+    @SerializedName("id")
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    @SerializedName("title")
+    var title: String = "",
+    @SerializedName("userId")
+    var userId: Int = 0
+)
