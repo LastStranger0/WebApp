4303cc53cd63ad2335179b15b63d7dd7ab1d7514
Anton Schur <aschur@scislt.com>
2022-12-13 18:46:12 +0300

    implemented livedata and viewmodel

--- app/src/main/java/com/example/webapp/MainActivity.kt
+++ app/src/main/java/com/example/webapp/MainActivity.kt
@@ -1,42 +1,29 @@
 package com.example.webapp
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val postsApi = PostRetrofit.getRetrofit()
-        val database = Room.
-        databaseBuilder(this.applicationContext,
-            PostDatabase::class.java,
-            "post")
-            .build()
+
 
         val button = findViewById<Button>(R.id.getPostButton)
         val textView = findViewById<TextView>(R.id.postsText)
-
+        viewModel.postsLiveData.observe(this) {
+            textView.text = it.toString()
+        }
         button.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.IO) {
-                val posts = postsApi.getPosts()
-                for (post in posts.body()!!) {
-                    database.getPostDao().insertPost(post)
-                }
-                val databasePosts = database.getPostDao().getAllPosts()
-                withContext(Dispatchers.Main){
-                    textView.text = databasePosts.toString()
-                }
-            }
+            viewModel.updatePostsFromDatabase(this)
         }
     }
 }
